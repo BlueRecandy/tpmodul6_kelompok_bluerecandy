5034077e77c8ffb7cc6c8a944e34033aeee24159
TiveCS <creativecode.id@gmail.com>
2022-03-30 21:44:15 +0700

    Add project files.

---
+++ tpmodul6_kelompok_bluerecandy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul6_kelompok_bluerecandy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
