cf8a2792f1f5630e1e76cd6189d4ef48dab0e405
luttfimohamad <luttfimohamad@gmail.com>
2022-03-31 17:49:52 +0700

    menambahkan class DataMahasiswa_1302204066

--- tpmodul6_kelompok_bluerecandy/Program.cs
+++ tpmodul6_kelompok_bluerecandy/Program.cs
@@ -7,14 +7,6 @@ namespace tpmodul6_kelompok_bluerecandy
     {
         static void Main(string[] args)
         {
-            // Demo DataMahasiswa Fathan
-            DataMahasiswa_1302204090 data = new DataMahasiswa_1302204090();
-            data.ReadJSON();
-
-            // Demo KuliahMahasiswa Fathan
-            KuliahMahasiswa_1302204090 kuliah = new KuliahMahasiswa_1302204090();
-            kuliah.ReadJSON();
-
             // Demo DataMahasiswa Lutfi
             DataMahasiswa_1302204066 data2 = new DataMahasiswa_1302204066();
             data2.ReadJSON();
