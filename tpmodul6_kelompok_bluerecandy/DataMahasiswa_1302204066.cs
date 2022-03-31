9cacc9ad1a3835797e9ce4b52d304362d6a4e458
TiveCS <creativecode.id@gmail.com>
2022-03-31 18:39:51 +0700

    fix merge lutfi post-DataMahasiswa

---
+++ tpmodul6_kelompok_bluerecandy/DataMahasiswa_1302204066.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul6_kelompok_bluerecandy
+{
+    internal class DataMahasiswa_1302204066
+    {
+        private string GetJsonPath => Path.GetFullPath(Directory.GetCurrentDirectory() + "/../../../tp6_1_1302204066.json");
+        public void ReadJSON()
+        {
+            var jsonFile = File.OpenText(GetJsonPath);
+            Dictionary<string, dynamic> json = JsonSerializer.Deserialize
+                <Dictionary<string, dynamic>>(jsonFile.ReadToEnd(), new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            });
+
+            Console.WriteLine("\n" + $"Nama {json["nama depan"]} {json["nama belakang"]} dengan nim {json["nim"]} dari fakultas {json["fakultas"]}");
+        }
+    }
+}
--- tpmodul6_kelompok_bluerecandy/Program.cs
+++ tpmodul6_kelompok_bluerecandy/Program.cs
@@ -6,7 +6,17 @@ namespace tpmodul6_kelompok_bluerecandy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Demo DataMahasiswa Fathan
+            DataMahasiswa_1302204090 data = new DataMahasiswa_1302204090();
+            data.ReadJSON();
+
+            // Demo KuliahMahasiswa Fathan
+            KuliahMahasiswa_1302204090 kuliah = new KuliahMahasiswa_1302204090();
+            kuliah.ReadJSON();
+
+            // Demo DataMahasiswa Lutfi
+            DataMahasiswa_1302204066 data2 = new DataMahasiswa_1302204066();
+            data2.ReadJSON();
         }
     }
 }
